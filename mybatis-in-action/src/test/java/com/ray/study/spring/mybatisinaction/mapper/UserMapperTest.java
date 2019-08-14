500709c6cc9c6d6e56b0737d83ca3f1fb0e55384
rayner shi <1766211120@qq.com>
2019-08-14 19:50:42 +0800

    [ADD] mybatis

--- mybatis-in-action/src/test/java/com/ray/study/spring/mybatisinaction/mapper/UserMapperTest.java
+++ mybatis-in-action/src/test/java/com/ray/study/spring/mybatisinaction/mapper/UserMapperTest.java
@@ -6,9 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -26,8 +24,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Transactional
-@Rollback
+//@Transactional
+//@Rollback
 public class UserMapperTest {
 
 	@Autowired
